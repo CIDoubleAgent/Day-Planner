2740b36d69f53c7e95ba0ed6b30b6c7309c3e8ec
Nick Zabkar <nzabkar51@gmail.com>
2021-08-24 00:28:03 -0400

    refactoring

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,54 @@
 
-var currentDate = moment().format("dddd, MMMM Do");
-var currentHour = moment().format('h:mm:ss a');
+let todaysDate = moment().format("dddd, MMMM Do");
+let currentHour = moment().format('h:mm:ss a');
+let userInput;
+let hourSpan;
+let hour = moment().hours();
+const nineAm = $("#9");
+const tenAm = $("#10");
+const elevenAm = $("#11");
+const twelvePm = $("#12");
+const onePm = $("#13");
+const twoPm = $("#14");
+const threePm = $("#15");
+const fourPm = $("#16");
+const fivePm = $("#17");
 
-var nineAm = $("#9am");
-var tenAm = $("#10am");
-var elevenAm = $("#11am");
-var twelvePm = $("#12pm");
-var onePm = $("#13pm");
-var twoPm = $("#14pm");
-var threePm = $("#15pm");
-var fourPm = $("#16pm");
-var fivePm = $("#17pm");
-var sixPm = $("#18pm");
-var sevenPm = $("#19pm");
+let interval = setInterval(function() {
+  $('#day').html(todaysDate);
+}, 1 * 100);
 
-var hour = moment().hours();
-var userInput;
-var hourSpan;
-
-var interval = setInterval(function() {
-  $('#currentDay').html(currentDate);
-}, 100);
-
-function init() {
-
-  console.log("Current Hour " + hour);
-  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
-  nineAm.val(init9);
-
-  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
-  tenAm.val(init10);
-  
-  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
-  elevenAm.val(init11);
-  
-  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
-  twelvePm.val(init12);
-  
-  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
-  onePm.val(init1);
-  
-  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
-  twoPm.val(init2);
-  
-  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
-  threePm.val(init3);
- 
-  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
-  fourPm.val(init4);
-  
-  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
-  fivePm.val(init5);
-  
-  var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
-  sixPm.val(init6);
-  
-  var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
-  sevenPm.val(init7);
+function initScheduler() {
+  const am9 = JSON.parse(localStorage.getItem("09:00 am"));
+  nineAm.val(am9);
+  const am10 = JSON.parse(localStorage.getItem("10:00 am"))
+  tenAm.val(am10);
+  const am11 = JSON.parse(localStorage.getItem("11:00 am"))
+  elevenAm.val(am11);
+  const pm12 = JSON.parse(localStorage.getItem("12:00 pm"))
+  twelvePm.val(pm12);
+  const pm1 = JSON.parse(localStorage.getItem("01:00 pm"))
+  onePm.val(pm1);
+  const pm2 = JSON.parse(localStorage.getItem("02:00 pm"))
+  twoPm.val(pm2);
+  const pm3 = JSON.parse(localStorage.getItem("03:00 pm"))
+  threePm.val(pm3);
+  const pm4 = JSON.parse(localStorage.getItem("04:00 pm"))
+  fourPm.val(pm4);
+  const pm5 = JSON.parse(localStorage.getItem("05:00 pm"))
+  fivePm.val(pm5);
 } 
 
-function background () {
-      
+function backgroundColor () {     
   $(".form-control").each(function () {
-      var timeTest = parseInt($(this).attr("id"));
+      let time = parseInt($(this).attr("id"));
       hour = parseInt(hour);
-      console.log(timeTest);
+      console.log(time);
       console.log(hour);
 
-      if (hour > timeTest) {
+      if (hour > time) {
           $(this).addClass("past");
-      } else if (hour < timeTest) {
+      } else if (hour < time) {
           $(this).addClass("future");
       } else {
           $(this).addClass("present");
@@ -78,21 +57,19 @@ function background () {
 }
 
 $(document).ready(function() {
-  init()
-  background()
+  initScheduler()
+  backgroundColor()
 
-  $(".saveBtn").on("click", function(){
+  $(".save").on("click", function(){
     userInput = $(this).siblings(".form-control").val().trim();
     console.log(userInput);
     hourSpan = $(this).siblings(".input-group-prepend").text().trim();
     console.log(hourSpan);
     localStorage.setItem(hourSpan, JSON.stringify(userInput));
-
   })
 
-  $("#clearDay").on("click", function(){
+  $("#clear").on("click", function(){
     localStorage.clear();
-    init()
+    initScheduler()
   }) 
-
 });
